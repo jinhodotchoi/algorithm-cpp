b9ddb1882ff3fcde56cf87ab753e246f9ff0287b
jinhdootchoi <leo.choi@lge.com>
2025-10-08 15:46:29 +0900

    feat: Submit 10808

--- questions/boj-10808.cpp
+++ questions/boj-10808.cpp
@@ -1,6 +1,7 @@
 /**
- * @copyright 2025
- * Backjoon boj-10808
+ * @copyright Copyright (c) 2025 jinho.choi
+ * @see https://www.acmicpc.net/problem/10808
+ * @license This code is for educational purposes only.
  */
 
 #include <bits/stdc++.h>
@@ -8,13 +9,23 @@
 using namespace std;
 
 void mainImpl() {
+    string str;
+    cin >> str;
+
+    vector<int> v(26, 0);
+
+    for (auto c : str) v[c - 'a']++;
+
+    for (auto i : v) cout << i << " ";
+
+    return;
 }
 
 int main() {
-    freopen("./boj-10808.txt", "r", stdin);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    // comment below line when submitting to boj website.
+    // freopen("./questions/boj-10808.txt", "r", stdin);
+    // for faster input/output
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
     mainImpl();
 
