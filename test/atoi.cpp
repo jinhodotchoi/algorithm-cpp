ea985556f7e8567b4018666189918c83c64fd621
jinhdootchoi <leo.choi@lge.com>
2025-10-09 19:29:36 +0900

    feat: Test atoi.cpp

---
+++ test/atoi.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string str = "123";
+    // NOTE: atoi 함수는 문자열을 정수로 변환해주는 함수다.
+    cout << atoi(str.c_str()) << endl;
+    return 0;
+}
